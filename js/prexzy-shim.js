09d2a581bafb5456eb2f4e5bbdd7fc815d5b51b2
Joseph Anih <awesomejoses11@gmail.com>
2026-08-15 12:44:59 +0100

    Shim: add runRoute stub

--- js/prexzy-shim.js
+++ js/prexzy-shim.js
@@ -9,6 +9,7 @@
       callResilient: function () { return Promise.reject(new Error('PrexzyAPI not loaded')); },
       generateImage: function () { return Promise.reject(new Error('PrexzyAPI not loaded')); },
       generateVideo: function () { return Promise.reject(new Error('PrexzyAPI not loaded')); },
+      runRoute: function () { return Promise.reject(new Error('PrexzyAPI not loaded')); },
       showLoading: function () { return { setMessage: function () {}, clear: function () {} }; },
       PrexzyError: function (kind, msg) { var e = new Error(msg); e.kind = kind; return e; }
     };

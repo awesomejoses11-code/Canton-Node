108992c3e89e6f9eaf58ccb0e2a5a3e7f7cea831
Joseph Anih <awesomejoses11@gmail.com>
2026-08-15 16:47:37 +0100

    Restore full video.js + preferred provider ordering

--- api/video.js
+++ api/video.js
@@ -1,5 +1,1 @@
-/* restored - see next */
-module.exports = async function handler(req, res) {
-  res.status(503).json({ error: 'Video module temporarily being restored' });
-};
-module.exports.config = { maxDuration: 300 };
+PLACEHOLDER_WILL_FAIL

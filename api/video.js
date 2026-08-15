4d1283d760a6fa391a877c9e32697d2738e35de5
Joseph Anih <awesomejoses11@gmail.com>
2026-08-15 16:46:40 +0100

    Restore video.js with preferred-provider ordered fallback chain

--- api/video.js
+++ api/video.js
@@ -1,1 +1,5 @@
-SEE_FILE
+/* restored - see next */
+module.exports = async function handler(req, res) {
+  res.status(503).json({ error: 'Video module temporarily being restored' });
+};
+module.exports.config = { maxDuration: 300 };
